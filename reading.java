c458f795bc94c578f882faf0e27d925cfd6efa6f
Layan946 <layanalmasoud4@gmail.com>
2024-11-08 15:07:03 +0300

    Add files via upload

---
+++ reading.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs212;
+
+/**
+ *
+ * @author Layan Almasoud
+ */
+import java.io.File;
+import java.util.Scanner;
+public class reading {
+    public static void Load(String fileName){
+        String line=null;
+       try{
+           File f = new File(fileName);
+           Scanner s = new Scanner(f);
+           s.nextLine();
+           while(s.hasNextLine()){
+               line=s.nextLine();
+               
+               if(line.trim().length()<3){
+                   System.out.println("Empty line found, skipping this line= "+line);
+                   break;
+               }
+               System.out.println(line);
+               String x = line.substring(0,line.indexOf(','));
+               int id = Integer.parseInt(x.trim());
+               String content = line.substring(line.indexOf(',')+1).trim();
+           }
+       }catch(Exception e){
+           System.out.println("end of file");
+       }
+    }
+}
