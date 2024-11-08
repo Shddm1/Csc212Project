4d31612c7e98d5f456c5e69c368b613edc19d84f
Shddm1 <162800828+Shddm1@users.noreply.github.com>
2024-11-08 17:09:08 +0300

    Update invertedIndex.java

--- invertedIndex.java
+++ invertedIndex.java
@@ -9,5 +9,18 @@ package cs212;
  * @author Layan Almasoud
  */
 public class invertedIndex {
-    
+     public void add(String text , int id ) {
+        Word w = new Word (text) ;
+        if(!exist(text)){
+                     Word w = new Word(text) ;
+                     w.IDs.insert(id);
+                     invertedIndex.insert(w)
+                             }
+        else {
+            Word existword = invertedIndex.retrieve():
+            existword.AddID(id);
+            
+
+        }
+    }
 }
